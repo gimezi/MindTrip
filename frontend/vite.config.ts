0844a5c0a5c529c00e6d23687bf2d1ed4c5aa437
yeji <yeji1107@naver.com>
2024-04-17 15:51:59 +0900

    [feat] PWA 적용해보기

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {'global': {} },
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'PWA TEST',
+        short_name: 'pwa',
+        description: '테스트용앱',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+
+    })
+  ],
+  define: { 'global': {} },
 })
